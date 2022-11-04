2382d78f7ac0c8e28165aeba8c1a4de36d6bd4e1
0xNull <45806668+danysite@users.noreply.github.com>
2022-11-04 08:49:40 +0700

    Create freedomgrep.sh

---
+++ freedomgrep.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Enter the Query: "  
+read payload  
+
+for QUERY in $(echo $payload);do mkdir ${QUERY%} > /dev/2 null>&1 ./${QUERY%}/${QUERY%}-VULN > /dev/null 2>&1;
+curl -s "https://beta.shodan.io/search/facet?query=$QUERY&facet=ip" | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' > ./${QUERY%}/${QUERY%}-ip-$(date +%Y-%m-%d).txt;
+curl -s "https://beta.shodan.io/search/facet?query=$QUERY&facet=port" | grep -Eo '<strong>[0-9]{1,4}' | tr -d "<strong>" | sed 's/$/, /g' | xargs echo | sed 's/,$//g' | tr -d " "  > ./${QUERY%}/${QUERY%}-port-$(date +%Y-%m-%d).txt;
+cat ./${QUERY%}/${QUERY%}-ip-$(date +%Y-%m-%d).txt | anew ./${QUERY%}/${QUERY%}-ip-new-$(date +%Y-%m-%d).txt;
+cat ./${QUERY%}/${QUERY%}-port-$(date +%Y-%m-%d).txt  | anew ./${QUERY%}/${QUERY%}-port-new-$(date +%Y-%m-%d).txt;
