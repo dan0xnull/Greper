c3d2bef235ebd4bd10318b97146a2452730a4234
0xNull <45806668+danysite@users.noreply.github.com>
2023-01-31 13:23:23 +0700

    Update recon-tools.sh

--- recon-tools.sh
+++ recon-tools.sh
@@ -1,12 +1,12 @@
-#install depedencies
+#install depedencies in ubuntu server
 
 wget https://go.dev/dl/go1.19.5.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.5.linux-amd64.tar.gz
-mkdir /home/haklog/go
+mkdir /root/haklog/go
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
-echo "export PATH=$PATH:/home/haklog/go/bin" >> ~/.bashrc
-
-########################################################################
+echo "export PATH=$PATH:/root/haklog/go/bin" >> ~/.bashrc
+source .bashrc
+apt install gcc
 go install -v github.com/tomnomnom/anew@latest
 go install github.com/tomnomnom/waybackurls@latest
 go install github.com/lc/gau/v2/cmd/gau@latest
